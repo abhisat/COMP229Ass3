3c2e848e9f0242cbe5e5c152fb35fa111c3782dd
Matt Roberts <matthew.roberts@mq.edu.au>
2017-08-11 10:43:46 +1000

    Task 6 Solution

--- src/Main.java
+++ src/Main.java
@@ -6,40 +6,17 @@ public class Main extends JFrame implements Runnable {
 
     private class Canvas extends JPanel {
 
-        private static final int MARGIN = 10;
-        private static final int WIDTH = 35;
-        private static final int HEIGHT = 35;
-
-        private Point[][] positions = new Point[20][20];
+        private Grid grid;
 
         public Canvas() {
             setPreferredSize(new Dimension(1280, 720));
 
-            for (int x = 0; x < positions.length; x++) {
-                for (int y = 0; y < positions[x].length; y++){
-                    positions[x][y] = new Point(MARGIN + x * WIDTH, MARGIN + y * HEIGHT);
-                }
-            }
+            grid = new Grid(10, 10);
         }
 
         @Override
         public void paint(Graphics g) {
-            for(int x = 0; x < positions.length; x++){
-                for(int y = 0; y < positions[x].length; y++){
-                    if (isWithin(getMousePosition(), positions[x][y], new Dimension(WIDTH, HEIGHT))){
-                        g.setColor(Color.LIGHT_GRAY);
-                        g.fillRect(positions[x][y].x, positions[x][y].y, WIDTH, HEIGHT);
-                    }
-                    g.setColor(Color.BLACK);
-                    g.drawRect(positions[x][y].x, positions[x][y].y, WIDTH, HEIGHT);
-                }
-            }
-        }
-
-        public boolean isWithin(Point target, Point topLeft, Dimension area){
-            if (target == null)
-                return false;
-            return target.x > topLeft.x && target.x < topLeft.x + area.width && target.y > topLeft.y && target.y < topLeft.y + area.width;
+            grid.paint(g, getMousePosition());
         }
     }
 
