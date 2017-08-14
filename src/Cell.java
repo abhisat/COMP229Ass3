f694f2108d413b19ed1ff1d1bdee45489fd46006
Matt Roberts <matthew.roberts@mq.edu.au>
2017-08-14 14:17:49 +1000

    Task 7 solution

--- src/Cell.java
+++ src/Cell.java
@@ -1,13 +1,9 @@
 import java.awt.*;
 
-public class Cell {
-
-    int x;
-    int y;
+public class Cell extends Rectangle {
 
     public Cell(int x, int y) {
-        this.x = x;
-        this.y = y;
+        super(x, y, 35, 35);
     }
 
     public void paint(Graphics g, Boolean highlighted) {
@@ -20,9 +16,10 @@ public class Cell {
         g.drawRect(x, y, 35, 35);
     }
 
+    @Override
     public boolean contains(Point target){
         if (target == null)
             return false;
-        return target.x > x && target.x < x + 35 && target.y > y && target.y < y +35;
+        return super.contains(target);
     }
 }
